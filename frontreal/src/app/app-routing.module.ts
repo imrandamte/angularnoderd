b67610de30e36c3d5d7286d6280f71fc53ece378
Damte Imran <110685655+imrandamte@users.noreply.github.com>
2024-04-06 05:19:22 +0530

    s commit

---
+++ frontreal/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { CategorycreateComponent } from './categorycreate/categorycreate.component';
+
+const routes: Routes = [
+  {path:'create',component:CreateComponent},
+  {path:'create/:id',component:CreateComponent},
+  {path:'read',component:ReadComponent},
+  {path:'categorycreate',component:CategorycreateComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
